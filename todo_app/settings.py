de076ef22c8fef23ff6954c0aeef4c63149e35e3
Blairnation <tonyblair246@gmail.com>
2024-10-13 15:32:02 +0000

    todo modified

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ve73(aa_m7x4g$f-&ami81k^ode07x$a_0@+t-7qsr^n!g24j_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todo-app-f4ml.onrender.com',
     'localhost',  
